3d44ae13ed94cc8f4fa80a7d968b54deb8b094f4
Beshoy Fomail <fomailbeshoy@gmail.com>
2025-11-24 23:29:52 +0200

    app edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,66 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+// import { AuthProvider } from './context/AuthContext';
 
 // Styles
-// Note: We removed 'all.css' because you are using the CDN link in index.html
 import './styles/global.css';
 
 // Components
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// Pages (IMPORT THE REAL FILES HERE)
+// Pages
+import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import CartPage from './pages/CartPage';
-import HomePage from './pages/HomePage';             // <--- Real File
-import BookDetailsPage from './pages/BookDetailsPage'; // <--- Real File
+import BookDetailsPage from './pages/BookDetailsPage';
 import LoginPage from './pages/LoginPage';
+
+// Missing Pages (Commented out until you create them)
+// import CategoriesPage from './pages/CategoriesPage';
+// import BooksPage from './pages/BooksPage';
+// import ContactPage from './pages/ContactPage';
+// import CheckoutPage from './pages/CheckoutPage';
+
+
+// 1. Layout Component
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 function App() {
   return (
-    <Router>
-      <div className="page">
-        <Header />
-        
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/book-details" element={<BookDetailsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          
-        </Routes>
+    /* <AuthProvider>  <-- REMOVED: Because you don't have the context yet */
+      <Router>
+        <div className="page">
+          <Routes>
+            
+            {/* GROUP 1: Pages WITH Header & Footer */}
+            <Route element={<MainLayout />}>
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/book-details/:id" element={<BookDetailsPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              
+              {/* These are commented out to prevent crashing */}
+              {/* <Route path="/categories" element={<CategoriesPage />} /> */}
+              {/* <Route path="/books" element={<BooksPage />} /> */}
+              {/* <Route path="/checkout" element={<CheckoutPage />} /> */}
+              {/* <Route path="/contact" element={<ContactPage />} /> */}
+            </Route>
 
-        <Footer />
-      </div>
-    </Router>
+            {/* GROUP 2: Pages WITHOUT Header & Footer (Clean) */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<HomePage />} />
+
+          </Routes>
+        </div>
+      </Router>
+    /* </AuthProvider> <-- REMOVED */
   );
 }
 
